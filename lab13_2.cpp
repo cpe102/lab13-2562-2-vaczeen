8c264b6ddcd0e0010c6ab5f66c9b699c8ba8fbdd
lab302-31 <lab302-31@labeng.local>
2020-02-25 14:51:16 +0700

    lab13.2

--- lab13_2.cpp
+++ lab13_2.cpp
@@ -1,5 +1,4 @@
-#include<iostream
-#include<string>
+#include<iostream>
 using namespace std;
 
 const int N = 5;
@@ -12,7 +11,7 @@ void showMatrix(const bool [][N]);
 
 int main(){
 	double A[N][N]; 
-	bool B[N][N]; 
+	bool B[N][N]={}; 
 	cout << "Input the matrix.....\n";
 	inputMatrix(A);
 	findLocalMax(A,B);
@@ -20,43 +19,37 @@ int main(){
 	showMatrix(B);
 	return 0;
 }
+
 void inputMatrix(double A[][N]){
-
 	for(int i=0;i<N;i++){
-	cout<<"Row "<<i+1<<":";
-	for(int j=0;j<N;j++){
-		cin>>A[i][j];
-	}
-	
-	}
-
-}
-void findLocalMax(const double A[][N], bool B[][N]){
-	B[0][0]=0;
-	B[0][1]=0;
-	B[0][2]=0;
-	B[0][3]=0;
-	B[0][4]=0;
-	B[1][0]=0;
-	B[2][0]=0;
-	B[3][0]=0;
-	B[4][0]=0;
-	B[4][1]=0;
-	B[4][2]=0;
-	B[4][3]=0;
-	B[4][4]=0;
-	B[2][4]=0;
-	B[3][4]=0;
-	for(int i=0;i<N;i++){
+		cout << "Row "<< i+1 <<" : ";
 		for(int j=0;j<N;j++){
-		if(A[i][j]>=A[i][j+1]&&A[i][j]>=A[i+1][j]&&A[i][j]>=A[i+2][j+1]&&A[i][j]>=A[i+1][j+2]){
-		return B[i][j]1;
-	}else{
-		return B[i][j]0;
-	}	
+			cin >> A[j][i];
 		}
 	}
 	
 	
 }
-// Wri
+
+void findLocalMax(const double A[][N], bool B[][N]){
+	for(int i=1;i<4;i++){
+		for(int j=1;j<4;j++){
+			if(A[j][i]>A[j-1][i] && A[j][i]>A[j+1][i] && A[j][i]>A[j][i-1] && A[j][i]>A[j][i+1]){
+			B[j][i] = 1;
+			B[j+1][i] = 0;
+			B[j-1][i] = 0;
+			B[j][i-1] = 0;
+			B[j][i+1] = 0;
+			}
+		}
+	}
+
+}
+
+void showMatrix(const bool B[][N]){
+	for(int i=0;i<N;i++){
+		for(int j=0;j<N;j++){
+			cout << B[j][i] << " ";
+		}
+		cout << "\n";
+	}
